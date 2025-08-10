a8dd756fc43e6d2ec8c04e0feee1bed0a926300a
Hemant <hemantnawghare13@gmail.com>
2025-08-10 13:36:10 +0530

    Updated feature X / Fixed bug Y

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb+srv://mrunmaynawghare:Munnabhai13@cluster0.nldsq.mongodb.net/food-del").then(() => console.log("DB Connnected"));
+        await mongoose.connect(process.env.MONGO_KEY).then(() => console.log("DB Connnected"));
     } catch (error) {
         console.log("connection on database has been failed", error);
         process.exit(1);
